f227336c494e36a80a588811445768fbbde172d0
giselaimera <imeragisela@gmail.com>
2025-01-23 16:19:27 +0100

    Files linked and structure of script.js created

---
+++ script.js
@@ -0,0 +1,18 @@
+function addNumber(num1,num2){
+    return num1+num2;
+}
+
+function subtractNumber(num1,num2){
+    return num1-num2;
+}
+
+function multiplyNumber(num1,num2){
+    return num1*num2;
+}
+
+function divideNumber(num1,num2){
+    return num1/num2;
+}
+function operate(){
+
+}
