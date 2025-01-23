225c0204c440cb685fd7365564159ac1ca5f6162
giselaimera <imeragisela@gmail.com>
2025-01-23 18:15:01 +0100

    Implemented number and operator button functionality

--- script.js
+++ script.js
@@ -11,8 +11,62 @@ function multiplyNumber(num1,num2){
 }
 
 function divideNumber(num1,num2){
+    if (num2===0){
+        return "Cannot divide by zero!";
+    }
     return num1/num2;
 }
-function operate(){
-
+function remainderNumber(num1,num2){
+    return num1%num2;
 }
+
+function operate(num1,num2,operator){
+    switch(operator){
+        case "+":
+            return addNumber(num1,num2);
+        case "-":
+            return subtractNumber(num1,num2);
+        case "/":
+            return divideNumber(num1,num2);
+        case "*":
+            return multiplyNumber(num1,num2);
+        case "%":
+            return remainderNumber(num1,num2);
+        
+    }
+}
+function updateDisplay(value){
+    const display=document.querySelector(".result");
+    display.textContent=value;
+}
+let operators=document.querySelectorAll(".operator");
+const operator_list=[...operators];
+let numbers=document.querySelectorAll(".number");
+const number_list=[...numbers];
+let num1="";
+let num2="";
+let operator="";
+let result="";
+number_list.forEach((number)=>{
+number.addEventListener("click",()=>{
+    const clickedNumber=number.textContent;
+    if (operator===""){
+        num1+=clickedNumber;
+        updateDisplay(num1);
+    }
+    else{
+        num2+=clickedNumber;
+        updateDisplay(num2);
+
+    }
+});
+});
+
+operator_list.forEach((operator)=>{
+operator.addEventListener("click",()=>{
+    const clickedOperator=operator.textContent;
+    updateDisplay(clickedOperator);
+});
+});
+
+
